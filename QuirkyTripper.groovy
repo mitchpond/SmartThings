05777339bc4ef0f2e9cf3f081f53de08dbdf0d12
mitchpond <mitchp@gmail.com>
2015-01-07 22:02:59 -0600

    Update QuirkyTripper.groovy

--- QuirkyTripper.groovy
+++ QuirkyTripper.groovy
@@ -26,7 +26,7 @@ metadata {
         command "getBattery"
         command "configure"
         
-        fingerprint profileID: "0104", deviceId: "0402", endpointId: "01", deviceVersion: "00", inClusters: "0000 0001 0003 0500 0020 0B05", outClusters: "0003 0019"
+        fingerprint (profileId: "0104", deviceId: "0402", endpointId: "01", inClusters: "0000 0001 0003 0500 0020 0B05", outClusters: "0003 0019")
 	}
 
 	// simulator metadata
