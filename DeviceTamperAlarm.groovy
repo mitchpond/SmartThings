ec9d9a5d9741d31e3c41d2b3ac7e7d43157271eb
mitchpond <mitchp@gmail.com>
2015-01-14 10:05:44 -0600

    v0.001

--- DeviceTamperAlarm.groovy
+++ DeviceTamperAlarm.groovy
@@ -1,41 +1,26 @@
 /**
- *  Notify Me When
+ *  Device Tamper Alarm
  *
- *  Author: SmartThings
+ *  Author: Mitch Pond, SmartThings
  *  Date: 2013-03-20
  */
 definition(
-    name: "Notify Me When",
-    namespace: "smartthings",
-    author: "SmartThings",
-    description: "Get a push notification or text message when any of a variety of SmartThings is activated.  Supports motion, contact, acceleration, moisture and presence sensors as well as switches.",
-    category: "Convenience",
+    name: "Device Tamper Alarm",
+    namespace: "mitchpond",
+    author: "Mitch Pond",
+    description: "Receive notification when a device is tampered with. Currently supports Quirky Tripper.",
+    category: "Safety & Security",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Meta/window_contact.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Meta/window_contact@2x.png"
 )
 
 preferences {
-	section("Choose one or more, when..."){
-		input "motion", "capability.motionSensor", title: "Motion Here", required: false, multiple: true
-		input "contact", "capability.contactSensor", title: "Contact Opens", required: false, multiple: true
-		input "contactClosed", "capability.contactSensor", title: "Contact Closes", required: false, multiple: true
-		input "acceleration", "capability.accelerationSensor", title: "Acceleration Detected", required: false, multiple: true
-		input "mySwitch", "capability.switch", title: "Switch Turned On", required: false, multiple: true
-		input "mySwitchOff", "capability.switch", title: "Switch Turned Off", required: false, multiple: true
-		input "arrivalPresence", "capability.presenceSensor", title: "Arrival Of", required: false, multiple: true
-		input "departurePresence", "capability.presenceSensor", title: "Departure Of", required: false, multiple: true
-		input "smoke", "capability.smokeDetector", title: "Smoke Detected", required: false, multiple: true
-		input "water", "capability.waterSensor", title: "Water Sensor Wet", required: false, multiple: true
-	}
-	section("Send this message (optional, sends standard status message if not specified)"){
-		input "messageText", "text", title: "Message Text", required: false
+	section("Choose devices..."){
+		input "contact", "capability.contactSensor", title: "Devices supporting tamper", required: false, multiple: true
 	}
 	section("Via a push notification and/or an SMS message"){
 		input "phone", "phone", title: "Phone Number (for SMS, optional)", required: false
 		input "pushAndPhone", "enum", title: "Both Push and SMS?", required: false, options: ["Yes","No"]
-	}
-	section("Minimum time between messages (optional, defaults to every message)") {
-		input "frequency", "decimal", title: "Minutes", required: false
 	}
 }
 
@@ -51,34 +36,15 @@ def updated() {
 }
 
 def subscribeToEvents() {
-	subscribe(contact, "contact.open", eventHandler)
-	subscribe(contactClosed, "contact.closed", eventHandler)
-	subscribe(acceleration, "acceleration.active", eventHandler)
-	subscribe(motion, "motion.active", eventHandler)
-	subscribe(mySwitch, "switch.on", eventHandler)
-	subscribe(mySwitchOff, "switch.off", eventHandler)
-	subscribe(arrivalPresence, "presence.present", eventHandler)
-	subscribe(departurePresence, "presence.not present", eventHandler)
-	subscribe(smoke, "smoke.detected", eventHandler)
-	subscribe(smoke, "smoke.tested", eventHandler)
-	subscribe(smoke, "carbonMonoxide.detected", eventHandler)
-	subscribe(water, "water.wet", eventHandler)
+	subscribe(contact, "tamper.tampered", eventHandler)
 }
 
 def eventHandler(evt) {
-	if (frequency) {
-		def lastTime = state[evt.deviceId]
-		if (lastTime == null || now() - lastTime >= frequency * 60000) {
-			sendMessage(evt)
-		}
-	}
-	else {
-		sendMessage(evt)
-	}
+	sendMessage(evt)
 }
 
 private sendMessage(evt) {
-	def msg = messageText ?: defaultText(evt)
+	def msg = evt.descriptionText
 	log.debug "$evt.name:$evt.value, pushAndPhone:$pushAndPhone, '$msg'"
 
 	if (!phone || pushAndPhone != "No") {
@@ -89,37 +55,4 @@ private sendMessage(evt) {
 		log.debug "sending SMS"
 		sendSms(phone, msg)
 	}
-	if (frequency) {
-		state[evt.deviceId] = now()
-	}
-}
-
-private defaultText(evt) {
-	if (evt.name == "presence") {
-		if (evt.value == "present") {
-			if (includeArticle) {
-				"$evt.linkText has arrived at the $location.name"
-			}
-			else {
-				"$evt.linkText has arrived at $location.name"
-			}
-		}
-		else {
-			if (includeArticle) {
-				"$evt.linkText has left the $location.name"
-			}
-			else {
-				"$evt.linkText has left $location.name"
-			}
-		}
-	}
-	else {
-		evt.descriptionText
-	}
-}
-
-private getIncludeArticle() {
-	def name = location.name.toLowerCase()
-	def segs = name.split(" ")
-	!(["work","home"].contains(name) || (segs.size() > 1 && (["the","my","a","an"].contains(segs[0]) || segs[0].endsWith("'s"))))
 }
